5016281c8eaa333dc9e020f99b48c53199f87af2
cxkks <114303487+cxkks@users.noreply.github.com>
2022-11-23 13:34:09 +0800

    Delete 1.js

--- 1.js
+++
@@ -1,3 +0,0 @@
-🇺🇸 | Snell(overST) = snell, 198.211.24.130, 443, psk=7Ok2Vb84DDVK6704W0BjurCAzrXmgaE, version=4, reuse=true, shadow-tls-sni=www.globaltimes.cn, shadow-tls-password=tls+1s
-
-🇺🇸 | Snell(overST) = snell,     2607:f130:0:105:ff:ff:34e3:a59c, 443, psk=7Ok2Vb84DDVK6704W0BjurCAzrXmgaE, version=4, reuse=true, shadow-tls-sni=www.globaltimes.cn, shadow-tls-password=tls+1s
