c0b39a446b31ad92a0d2c834f3789ef8222fa4cf
cxkks <114303487+cxkks@users.noreply.github.com>
2022-11-24 19:12:40 +0800

    Delete fd.js

--- fd.js
+++
@@ -1,3 +0,0 @@
-#樊登读书
-^http[s]?:\/\/.+dushu.+(v101/content|book/v100/info|/play/duration|/v100/index).*$ url request-body "token":"[^"]+ request-body "token":"20221117rUwJa1id5I0oUDDYTJk
-^http[s]?:\/\/.+dushu.+(v101|v100|program/v100)\/(content|play/duration|index|list|userInfo|vipInfo|info|ceiltip|mainList|getMarketInfoByType) url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/FD.js
